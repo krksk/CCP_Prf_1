78a3464f397fc64f51dffa8ed1d36e836878f9d3
krksk <cooling343@gmail.com>
2019-09-30 02:09:11 +0900

    1

---
+++ source/example.js
@@ -0,0 +1,12 @@
+window.onscroll = function(){
+  var t = document.documentElement.scrollTop;
+    if(t>600){
+    title.style.animationPlayState = "running";
+    a1.style.animationPlayState = "running";
+    a2.style.animationPlayState = "running";
+    a3.style.animationPlayState = "running";
+    a4.style.animationPlayState = "running";
+    field.style.animationPlayState = "running";
+    }
+    monitor.innerHTML = t + "px";
+    }
